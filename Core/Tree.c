21cebd2e7942526b6c40c1bc52a34c2d367a0c6b
Thyago Oliveira <taothyago@gmail.com>
2019-11-19 11:50:16 -0300

    :gem: Add create and destroy tree functions

--- Core/Tree.c
+++ Core/Tree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../Headers/Tree.h"
 
 typedef struct node Node;
 
@@ -8,8 +9,27 @@ struct tree {
     Node * root;
 };
 
-struct Node {
+struct node {
     int value;
     Node * left;
     Node * right;
 };
+
+Tree * create() {
+    Tree * tree = malloc(sizeof(Tree));
+    tree->root = NULL;
+    return tree;
+}
+
+void destroy_tree_rec(Node * node) {
+    if(node != NULL) {
+        destroy_tree_rec(node->left);
+        destroy_tree_rec(node->right);
+        free(node);
+    }
+}
+
+void destroy_tree(Tree * tree) {
+    destroy_tree_rec(tree->root);
+    free(tree);
+}
