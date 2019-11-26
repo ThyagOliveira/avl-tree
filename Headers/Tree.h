0cda0ca5ea58642698d5ca9822d92324c7ad664b
Thyago Oliveira <taothyago@gmail.com>
2019-11-26 12:14:53 -0300

    :construction: Update header tree

--- Headers/Tree.h
+++ Headers/Tree.h
@@ -8,5 +8,7 @@ void destroy_tree(Tree * tree);
 void add_node(Tree * tree, int value);
 void remove_node(Tree * tree, int * value);
 void print_tree(Tree * tree);
+void load_file(Tree * tree);
 void save_file_in_order(Tree * tree, FILE * file);
+
 int height(Tree * tree);
